edf9c13cfbabd039357e85f4ff0e2dd3f9b1fa8a
johnmccloskey <john.j.mccloskey76@gmail.com>
2022-01-30 11:54:42 -0600

    Syntax, Types, and Variables ex

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,20 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World");
-        System.out.print("Hello World");
+        System.out.print("Hello World \n");
+
+
+        int myFavoriteNumber = 27;
+        System.out.println(myFavoriteNumber);
+
+
+        String myString = ("Today is Championship Sunday");
+        System.out.println(myString);
+
+
+
     }
 }
-
 //Error: Main method not found in class HelloWorld, please define the main method as:
 //   public static void main(String[] args)
 //or a JavaFX application class must extend javafx.application.Application (Changed main method...result!)
