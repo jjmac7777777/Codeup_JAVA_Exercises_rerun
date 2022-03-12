a7ecd66ad09455790726649a847dfa96800adffe
johnmccloskey <john.j.mccloskey76@gmail.com>
2022-03-12 15:34:19 -0600

    Bob Exf

---
+++ src/Bob.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        boolean pressOn = true;
+
+        while (pressOn){
+            bobAnswers();
+            pressOn = bobAnswers();
+        }
+    }
+
+    private static boolean bobAnswers(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Say something to Bob?");
+        String userResponse = scanner.nextLine().trim();
+
+        if (userResponse.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (userResponse.endsWith("!")){
+            System.out.println("EZ Dude...er, Dad");
+        } else if (userResponse.isEmpty()) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Uhhhhhhhhh!!!.");
+        }
+
+        System.out.println("Would you like to say something else to Bob?");
+        String response = scanner.next();
+        return response.equalsIgnoreCase("y");
+    }
+}
+
